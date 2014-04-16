e0bf45cdda50f9c8ca122a31949fc54c8ec42258
Bryan Brick <wbbrick@gmail.com>
2014-04-16 07:32:58 -0400

    Writing a few initial tests

--- test/models/app.spec.js
+++ test/models/app.spec.js
@@ -5,6 +5,14 @@ describe('App Model', function () {
 
     beforeEach(function () {
         this.AppModel = new D3Interpolator.Models.App();
+        this.scale =this.AppModel.get('majorMusicTransformArray')
     });
+    describe('Scale Checks', function() {
+        it('should have notes and corresponding numbers', function() {
+            _.keys(this.scale).should.contain('1');
+            _.values(this.scale).should.contain("C1");
+            this.scale[1].should.equal('D1')
+        })
+    })
 
 });
--- test/models/content.spec.js
+++ test/models/content.spec.js
@@ -5,6 +5,47 @@ describe('Content Model', function () {
 
     beforeEach(function () {
         this.ContentModel = new D3Interpolator.Models.Content();
+        this.dateGenerator = this.ContentModel.dateGenerator;
+        this.noteGenerator = this.ContentModel.noteGenerator;
+    });
+
+    describe('check initialized values', function() {
+        it('should have noteGenerator and dateGenerator function', function()
+           {
+               this.ContentModel.noteGenerator
+                   .should.be.an.instanceOf(Object);
+               this.ContentModel.dateGenerator
+                   .should.be.an.instanceOf(Object);
+           });
+        it('should have the textGenerator set', function()
+           {
+               this.ContentModel.get('textGenerator')
+                   .should.be.an.instanceOf(Object);
+           });
+        it('should have an attributes hash and a divHeight', function()
+           {
+               this.ContentModel.get('attributes')
+                   .should.be.an.instanceOf(Object);
+               this.ContentModel.get('divHeight')
+                   .should.exist;
+           });
+    });
+    describe('check the text generator outputs', function() {
+        it('should output dates given the right object', function() {
+            this.dateGenerator(5, 'orangeaverage',
+                               {
+                                   cleanDate :'Thu Feb 11 2005'
+                               } )
+                .should.equal('Thu Feb 11 2005');
+        });
+        it('should output nothing given an empty or incorrect object', function() {
+            this.dateGenerator(5, 'orangeaverage', {})
+                .should.equal('');
+            this.dateGenerator({})
+                .should.equal('');
+            this.dateGenerator()
+                .should.equal('');
+        });
     });
 
 });
